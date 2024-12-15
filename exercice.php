fffc14729ce733509d9b09296a1d69216672b9ae
Issoumaila <issoumailafofana83@gmail.com>
2024-12-15 14:49:40 +0000

    first commit

---
+++ exercice.php
@@ -0,0 +1,13 @@
+<?php
+//Exercice1
+echo"Information de l'utilisateur";
+$prenom=readline("Entrez votre Prenom");
+$nom=readline("Entrez votre Nom");
+$age=readline("Entrez votre age");
+echo"Bonjour" . "   ". $prenom . "  " . $nom."  " . "vous avez donc". "   ". $age
+
+//Exercice2
+
+
+
+?>
